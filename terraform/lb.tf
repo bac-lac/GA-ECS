b412bc167454dc2df378c85c5571f17d70cb6c43
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-09-21 14:13:57 -0400

    Update ports

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -76,7 +76,7 @@ resource "aws_lb_listener_rule" "web_client_rule" {
   listener_arn        = aws_lb_listener.https.arn
   action {
     type              = "forward"
-    target_group_arn  = aws_lb_target_group.ga_tg_443.arn
+    target_group_arn  = aws_lb_target_group.ga_tg_8443.arn
   }
   condition {
     host_header {
@@ -88,16 +88,16 @@ resource "aws_lb_listener_rule" "web_client_rule" {
   }
 }
 
-resource "aws_lb_target_group" "ga_tg_443" {
-  name        = "ga-tg-${var.ENV}-443"
-  port        = 443
+resource "aws_lb_target_group" "ga_tg_8443" {
+  name        = "ga-tg-${var.ENV}-8443"
+  port        = 8443
   protocol    = "HTTPS"
   target_type = "ip"
   vpc_id      = data.aws_vpc.vpc.id
   health_check {
     path      = "/"
     matcher   = "200,302"
-    port      = 443
+    port      = 8443
     protocol  = "HTTPS"
   }
   stickiness {
@@ -116,7 +116,7 @@ data "aws_lb" "ga_nlb"{
 
 resource "aws_lb_listener" "sftp" {
   load_balancer_arn   = data.aws_lb.ga_nlb.arn
-  port                = "22"
+  port                = "8022"
   protocol            = "TCP"
   default_action {
     type              = "forward"
@@ -127,14 +127,14 @@ resource "aws_lb_listener" "sftp" {
   }
 }
 
-resource "aws_lb_target_group" "ga_tg_22" {
-  name        = "ga-tg-${var.ENV}-22"
-  port        = 22
+resource "aws_lb_target_group" "ga_tg_8022" {
+  name        = "ga-tg-${var.ENV}-8022"
+  port        = 8022
   protocol    = "TCP"
   target_type = "ip"
   vpc_id      = data.aws_vpc.vpc.id
   health_check {
-    port      = 22
+    port      = 8022
     protocol  = "TCP"
   }
   tags = {
