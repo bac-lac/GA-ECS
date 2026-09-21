7444f971dc790fee06157bed91abd6c231b8ec1e
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-09-21 15:00:06 -0400

    Fix ports

--- terraform/lb.tf
+++ terraform/lb.tf
@@ -120,7 +120,7 @@ resource "aws_lb_listener" "sftp" {
   protocol            = "TCP"
   default_action {
     type              = "forward"
-    target_group_arn  = aws_lb_target_group.ga_tg_22.arn
+    target_group_arn  = aws_lb_target_group.ga_tg_8022.arn
   }
   tags = {
     Name = "SFTP-${var.ENV}"
