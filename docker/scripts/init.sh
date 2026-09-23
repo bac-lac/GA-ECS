78d29aac08e93ffdc451d8858a0b530f89550666
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2026-09-23 10:57:46 -0400

    Remove temporary entrypoint

--- docker/scripts/init.sh
+++ docker/scripts/init.sh
@@ -100,15 +100,6 @@ function configure() {
         cp -Rf /temp/custom/ "${opt_ga_folder}"/ghttpsroot/
     fi
 
-    # Replace move with remove for upgrade file to eliminate CVEs.
-    # echo "Replace move with remove for upgrade file"
-    # sed -i "s|mv upgrader/ga_upgrade.jar upgrader/ga_upgrade_complete.jar|rm upgrader/ga_upgrade.jar|g" /temp/entrypoint.sh
-    # sed -i "s|mv upgrader/ga_upgrade.jar upgrader/ga_upgrade_complete.jar|rm upgrader/ga_upgrade.jar|g" /usr/bin/entrypoint.sh
-
-    # Update hostname in entrypoint with SYSTEM_NAME (MFT-1 or MFT-2).
-    # echo "Update hostname in entrypoint"
-    # sed -i "s/\$HOSTNAME/\$SYSTEM_NAME/g" /temp/entrypoint.sh
-
     # Update the header's page with ECR image.
     echo "Update the header's page with ECR image"
     local meta_param1="<meta name=\"ECR_IMAGE\" content=\"${ECR_IMAGE}\" />"
